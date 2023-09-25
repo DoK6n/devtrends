13710e89d2ae98e00522b69c7b3fd8da62a27971
DoK6n <ehrbs2018@gmail.com>
2023-09-25 13:06:54 +0900

    test: test용 api client 추가

--- src/test/client.ts
+++ src/test/client.ts
@@ -1,3 +1,80 @@
+interface ApiConfig {
+  baseURL: string
+  prefix: string
+}
+
+abstract class AbstractApi {
+  constructor(protected config: ApiConfig) { }
+
+  buildUrl(endpoint: string, params?: Record<string, any>): URL {
+    const url = new URL(`${this.config.baseURL}${this.config.prefix}${endpoint}`)
+    if (params) {
+      url.search = new URLSearchParams(params).toString()
+    }
+    return url;
+  }
+
+  abstract get<T>(endpoint: string, params?: Record<string, any>): Promise<T>
+  abstract post<T>(endpoint: string, data: Record<string, any>): Promise<T>
+  abstract put<T>(endpoint: string, data: Record<string, any>): Promise<T>
+  abstract delete<T>(endpoint: string, params?: Record<string, any>): Promise<T>
+  abstract patch<T>(endpoint: string, data: Record<string, any>): Promise<T>
+}
+
+export class Api extends AbstractApi {
+  constructor(config: ApiConfig) {
+    super(config); // 부모 클래스의 생성자를 호출
+  }
+
+  async get<T>(endpoint: string, params?: Record<string, any>): Promise<T> {
+    const url = this.buildUrl(endpoint, params);
+    const request = new Request(url.toString(), { method: 'GET' });
+    // 실제 API 요청 코드를 여기에 작성하거나, request 객체를 반환합니다.
+    return request.json() as T
+  }
+
+  async post<T>(endpoint: string, data: Record<string, any>): Promise<T> {
+    const url = this.buildUrl(endpoint);
+    const request = new Request(url.toString(), {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    // 실제 API 요청 코드를 여기에 작성하거나, request 객체를 반환합니다.
+    return request.json() as T
+  }
+
+  async put<T>(endpoint: string, data: Record<string, any>): Promise<T> {
+    const url = this.buildUrl(endpoint);
+    const request = new Request(url.toString(), {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    // 실제 API 요청 코드를 여기에 작성하거나, request 객체를 반환합니다.
+    return request.json() as T
+  }
+
+  async delete<T>(endpoint: string, params?: Record<string, any>): Promise<T> {
+    const url = this.buildUrl(endpoint, params);
+    const request = new Request(url.toString(), { method: 'DELETE' });
+    // 실제 API 요청 코드를 여기에 작성하거나, request 객체를 반환합니다.
+    return request.json() as T
+  }
+
+  async patch<T>(endpoint: string, data: Record<string, any>): Promise<T> {
+    const url = this.buildUrl(endpoint);
+    const request = new Request(url.toString(), {
+      method: 'PATCH',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    // 실제 API 요청 코드를 여기에 작성하거나, request 객체를 반환합니다.
+    return request.json() as T
+  }
+}
+
+
 export const reqBase = (path: string, options?: RequestInit) =>
   new Request(`http://localhost${path}`, options)
 
