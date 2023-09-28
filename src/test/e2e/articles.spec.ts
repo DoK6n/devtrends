b211eac903b99e0ab794a71a3472d7e7911b07c7
Dok6n <ehrbs2018@gmail.com>
2023-09-28 20:53:04 +0900

    test: 테스트용 API Client를 @elysiajs/eden로 변경
    - Request로 작성하였던 API Client 제거

--- src/test/e2e/articles.spec.ts
+++ src/test/e2e/articles.spec.ts
@@ -1,61 +1,66 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { beforeEach, describe, expect, it } from 'bun:test'
-import { Req } from '../client'
 import { Article } from 'src/repositories'
 import Elysia from 'elysia'
 import { database } from 'src/database'
 import { articlesRoute } from 'src/routes'
 
+import { edenTreaty } from '@elysiajs/eden'
+
 describe('E2E articles test', () => {
   const _serve = { serve: { hostname: 'localhost' } }
   // TODO 테스트 종료 후 테스트용 데이터 제거
 
-  const request = new Req({ baseURL: 'http://localhost', prefix: '/v1' })
+  const PORT = Bun.env.PORT || 8001
 
   const app = new Elysia(_serve)
     .get('/', () => 'healthy')
     .use(database)
     .use(articlesRoute())
+    .listen(PORT)
+
+  const client = edenTreaty<typeof app>(`http://localhost:${PORT}`)
 
   describe('health check', () => {
     it('health check', async () => {
-      const response = await app.handle(request._get('/'))
-
-      expect(await response.text()).toBe('healthy')
+      const response = await client.get()
+      expect(response.data).toBe('healthy')
     })
   })
 
   let article: Article
 
-  /**
-   * 테스트용 게시글 생성
-   */
-  beforeEach(async () => {
-    const response = await app.handle(
-      request.post('/articles/add', {
+  describe('실제 데이터 테스트', () => {
+    /**
+     * 테스트용 게시글 생성
+     */
+    beforeEach(async () => {
+      const { data, error } = await client.v1.articles.add.post({
         title: 'Elysia!!',
         author: 'dok6n',
-      }),
-    )
+      })
 
-    if (!response.ok) throw new Error('Article 생성 실패')
+      if (error) {
+        throw new Error('Article 생성 실패')
+      }
 
-    article = (await response.json()).data
-  })
-
-  describe('GET /v1/articles', () => {
-    it('전체 게시글 조회 테스트', async () => {
-      const response = await app.handle(request.get('/articles'))
-
-      expect((await response.json()).data instanceof Array).toStrictEqual(true)
+      article = data.data
     })
-  })
 
-  describe('GET /v1/articles/:articlesId', () => {
-    it('게시글 조회 테스트', async () => {
-      const response = await app.handle(request.get(`/articles/${article.id}`))
+    describe('GET /v1/articles', () => {
+      it('전체 게시글 조회 테스트', async () => {
+        const { data } = await client.v1.articles.get()
 
-      expect(await response.json()).toStrictEqual({ data: article })
+        expect(data?.data instanceof Array).toStrictEqual(true)
+      })
+    })
+
+    describe('GET /v1/articles/:articlesId', () => {
+      it('게시글 조회 테스트', async () => {
+        const { data } = await client.v1.articles[article.id].get()
+
+        expect(data).toStrictEqual({ data: article })
+      })
     })
   })
 })
